83976116f85527145b0a7ebcc120d83b379ffab8
Humble Bumble <157408786+HumbleBumbleBoy@users.noreply.github.com>
2026-05-17 15:04:55 +0200

    hotfix removed singular slash

--- src/server/server.ts
+++ src/server/server.ts
@@ -79,7 +79,7 @@ app.post('/api/user-login', async (req, res) => {
   }
 });
 
-app.get('/*', (_req, res) => {
+app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, '../../dist/index.html'));
 });
 
