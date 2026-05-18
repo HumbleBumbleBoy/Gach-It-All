cfd9e38c721e00a2f4a9db40756ad7697e8245c8
Humble Bumble <157408786+HumbleBumbleBoy@users.noreply.github.com>
2026-05-18 12:02:10 +0200

    Update server.ts

--- src/server/server.ts
+++ src/server/server.ts
@@ -79,6 +79,10 @@ app.post('/api/user-login', async (req, res) => {
 
 app.use(express.static(path.join(__dirname, '../../dist')));
 
+app.use((_req, res) => {
+  res.sendFile(path.join(__dirname, '../../dist/index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
